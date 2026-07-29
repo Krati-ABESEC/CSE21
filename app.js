67fa9d79ac773a021e5510da8d6c9f78770d8c1d
Krati Agarwal <kratiagarwal7812@gmail.com>
2026-07-29 09:48:55 +0530

    ES6

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ p("World!");
 // DOM won't run in the terminal using node.js
 // synchronous
 
+/*
 async function synous(){
     await setTimeout(()=>{// syntax of setTimeout is setTimeOut(arrow function, time in millisecs)
         console.log("its a synchronous programming");
@@ -65,6 +66,64 @@ function text(){
 
 display(text);
 
+// functions of array- filter, map, reduce , forEach
+
+let fruits= ["mango", "apple", "litchi", "Strawberry"];
+
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
+
+fruits.map((fruit)=>{
+    console.log(fruit);
+});
+
+// promises
+
+let promise= new Promise((resolve, reject)=>{
+    console.log("promise is pending");
+    resolve("operational success");//resolve and reject are the functions used as parameters in the promise
+})
+
+let choice= true;
+
+if(choice){
+    promise.then(res =>{
+        console.log("success");
+    })
+}else{
+    promise.catch(err =>{
+        console.log("something went wrong");
+    })
+}
+
+//
+function display(username, password, err){
+    return new Promise((resolve, reject)=>{
+        if(!err){
+            resolve(`username: ${username} and password: ${password}`);
+        }else{
+           reject("unable to fetch the info"); 
+        }
+    })
+}
+
+let np= display("Krati", "7812", false);
+
+np.then(res=> console.log(res)).catch(err => console.log(err));
+*/
+
+// async await is used to resolve the promise chaining problem
+
+async function test(){
+    console.log("1");
+    console.log("3");
+    await console.log("2");
+    console.log("4");
+}
+
+test();
+console.log(6);
 
 
 
